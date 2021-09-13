3c5da42cc5d89256769992eb13db490668c6e9bf
MSI\mr_bo <mr_boot@163.com>
2021-09-13 14:42:37 +0800

    配置中心动态刷新支持

--- SpringCloudDemo/spring-cloud-config-client/src/main/java/com/liuwei/learn/config/controller/ConfigController.java
+++ SpringCloudDemo/spring-cloud-config-client/src/main/java/com/liuwei/learn/config/controller/ConfigController.java
@@ -1,6 +1,7 @@
 package com.liuwei.learn.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description：
  */
 @RestController
+@RefreshScope //curl -X POST "http://localhost:8889/actuator/refresh" 通过命令调用监控的refresh进行刷新
 public class ConfigController {
     @Value("${config.info}")
     private String configInfo;
