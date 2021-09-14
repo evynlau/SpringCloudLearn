c2eb598dd99634258f5a1db25e1caab10a784db0
MSI\mr_bo <mr_boot@163.com>
2021-09-14 17:34:21 +0800

    消息消费分组配置添加

---
+++ SpringCloudDemo/spring-cloud-stream-consumer2/src/main/java/com/liuwei/learn/stream/StreamConsumerApplication.java
@@ -0,0 +1,18 @@
+package com.liuwei.learn.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author ：LiuWei
+ * @Date ：Created in 2021/9/14 16:52
+ * @Package: com.liuwei.learn
+ * @Modified By：
+ * @Description：
+ */
+@SpringBootApplication
+public class StreamConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumerApplication.class,args);
+    }
+}
